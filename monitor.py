a52a165e79cb2bbe3674a1e24b7d777c967e9eb5
James <contractorwolf@hotmail.com>
2015-12-06 05:00:43 +0000

    added more button events, corrected the photo ones action on multi press

--- monitor.py
+++ monitor.py
@@ -10,17 +10,21 @@ import dot3k.backlight as backlight
 import time, datetime, copy, math, psutil
 import dot3k.joystick as j
 import picamera
-import socket
+import os
 import subprocess
 
 screen_on = True;
 image_num = 0;
+picture_waiting = False;
+
+reset_initiated = False;
 
 @j.on(j.UP)
 def handle_up(pin):
     global screen_on
+    global reset_initiated
     print("Up pressed!")
-    print(screen_on)
+    #print(screen_on)
 
     if screen_on:
         backlight.rgb(0, 0, 0)
@@ -29,20 +33,53 @@ def handle_up(pin):
         backlight.rgb(200, 200, 200)
         screen_on = True
 
-
+    reset_initiated = False
 
 
 @j.on(j.BUTTON)
 def handle_button(pin):
     print("Button pressed!")
     global image_num
-    
-    image_num = image_num + 1
-    camera.capture('image' + format(image_num,'03') + '.jpg')
-    print 'image file: image' + format(image_num,'03') + '.jpg'
+    global reset_initiated
+    global picture_waiting
 
-    lcd.set_cursor_position(13, 2)
-    lcd.write(format(image_num,'03'))
+    if picture_waiting!=True:
+        picture_waiting = True
+        image_num = image_num + 1
+        camera.capture('image' + format(image_num,'03') + '.jpg')
+        print 'image file: image' + format(image_num,'03') + '.jpg'
+
+        lcd.set_cursor_position(13, 2)
+        lcd.write(format(image_num,'03'))
+        reset_initiated = False
+        picture_waiting = False
+    else:
+        print 'waiting on image to return'
+
+
+@j.on(j.DOWN)
+def handle_down(pin):
+    print("Down pressed!")    
+    global image_num
+    global reset_initiated
+
+    if reset_initiated == False:
+        reset_initiated = True
+        print("first time")
+    else:
+        lcd.set_cursor_position(0, 0)
+        lcd.write("SHUTTING DOWN   ")
+        os.system("sudo shutdown now");
+
+
+@j.on(j.LEFT)
+def handle_left(pin):
+    print("LEFT pressed!")
+
+    
+@j.on(j.RIGHT)
+def handle_right(pin):
+    print("RIGHT pressed!")
 
 
 def get_ip_addr():
@@ -58,7 +95,9 @@ ip = get_ip_addr()
 
 print "IP: " + ip
 
-lcd.write("IP:" + ip)
+lcd.set_cursor_position(0, 0)
+lcd.write("IP: " + ip)
+
 lcd.set_cursor_position(0, 1)
 lcd.write("CPU: ")
 
